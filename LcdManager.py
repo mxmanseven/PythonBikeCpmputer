c69d2ce5403b6f6c1a5269db4133902db38fe05d
Karl Hess <karlh@alpha-bli.com>
2018-03-01 08:29:03 -0800

    In LcdManager.py: * changed tabs to spaces * set max speed to 99.

--- LcdManager.py
+++ LcdManager.py
@@ -10,7 +10,7 @@ from EnduroScreenModel import EnduroScreenModel
 class LcdManager:
 
     def __init__(self):
-		self.lcd = CharLCD('PCF8574', 0x27)
+        self.lcd = CharLCD('PCF8574', 0x27)
 
 
     def setEnduroScreenByModel(self, enduroScreenModel):
@@ -25,95 +25,104 @@ class LcdManager:
         )
 
     def setEnduroScreen(
-		self, 
-		distance, 
-		milesToNextPossable,
-		routeSpeed, 
-		averageSpeed,  
-		currentSpeed,
-		timeTotalSeconds,
-		paceTotalSeconds):
-		# knh todo - limit speed to 2 digits
-		paceMinutes = int(abs(paceTotalSeconds / 60.0))
-		paceSeconds = int(abs(paceTotalSeconds % 60))
-		if paceTotalSeconds < 0:
-			paceMinutes = floor(abs(paceTotalSeconds / 60.0))
-			paceSeconds = int(abs(paceTotalSeconds) % 60)
-		
-		if paceMinutes > 10:
-			paceMinutes = 9
-			paceSeconds = 59
+        self, 
+        distance, 
+        milesToNextPossable,
+        routeSpeed, 
+        averageSpeed,  
+        currentSpeed,
+        timeTotalSeconds,
+        paceTotalSeconds):
+    
+        # knh todo - limit speed to 2 digits
+        paceMinutes = int(abs(paceTotalSeconds / 60.0))
+        paceSeconds = int(abs(paceTotalSeconds % 60))
+        if paceTotalSeconds < 0:
+            paceMinutes = floor(abs(paceTotalSeconds / 60.0))
+            paceSeconds = int(abs(paceTotalSeconds) % 60)
+        
+        if paceMinutes > 10:
+            paceMinutes = 9
+            paceSeconds = 59
 
-		paceSign = "+"
-		if paceTotalSeconds < 0:
-			paceSign = "-"
+        paceSign = "+"
+        if paceTotalSeconds < 0:
+            paceSign = "-"
 
-		sys.stdout.write('paceTotalSeconds ' + str(paceTotalSeconds) + '\r\n')
-		sys.stdout.write('paceMinutes ' + str(paceMinutes) + '\r\n')
-		sys.stdout.write('paceSeconds ' + str(paceSeconds) + '\r\n')
-		sys.stdout.write('\r\n')
+        sys.stdout.write('paceTotalSeconds ' + str(paceTotalSeconds) + '\r\n')
+        sys.stdout.write('paceMinutes ' + str(paceMinutes) + '\r\n')
+        sys.stdout.write('paceSeconds ' + str(paceSeconds) + '\r\n')
+        sys.stdout.write('\r\n')
 
-		line1 = ("D{:5.2f}   P{}{:01.0f}:{:02d}   {:2.0f}\r\n"
-		.format(distance, paceSign, paceMinutes, paceSeconds, routeSpeed))
+        line1 = ("D{:5.2f}   P{}{:01.0f}:{:02d}   {:2.0f}\r\n"
+        .format(distance, paceSign, paceMinutes, paceSeconds, routeSpeed))
+    
+        if averageSpeed > 99:
+          averageSpeed = 99
 
-		line2 = "{:20.0f}\r\n".format(averageSpeed)
+        line2 = "{:20.0f}\r\n".format(averageSpeed)
 
-		timeMinutes = int(round(timeTotalSeconds / 60.0)) % 60
-		timeSeconds = int(round(timeTotalSeconds % 60))
-		
-		milesToNextPossableAdjusted = milesToNextPossable
-		if milesToNextPossable > 10:
-			milesToNextPossableAdjusted = 9.9
+        timeMinutes = int(round(timeTotalSeconds / 60.0)) % 60
+        timeSeconds = int(round(timeTotalSeconds % 60))
+        
+        milesToNextPossableAdjusted = milesToNextPossable
+        if milesToNextPossable > 10:
+            milesToNextPossableAdjusted = 9.9
+      
+        if currentSpeed > 99:
+          currentSpeed  = 99
 
-		line3 = ("T {:02.0f}:{:02d}  NP{:4.1f} {:4.0f}\r\n"
-		.format(timeMinutes, timeSeconds, milesToNextPossableAdjusted, currentSpeed))
+        line3 = ("T {:02.0f}:{:02d}  NP{:4.1f} {:4.0f}\r\n"
+        .format(timeMinutes, timeSeconds, milesToNextPossableAdjusted, currentSpeed))
 
-		# knh todo - get rounding correct 
+        # knh todo - get rounding correct 
 
-		# always round away from zero
-		paceMarkerCount = int(ceil(abs(paceTotalSeconds / 15.0)))
-			
-		# We will display at most 9 pace chars
-		if paceMarkerCount > 9:
-			paceMarkerCount = 9
-		# we have 20 chars, those that are not pace markers are spaces
-		paddCharCount = 20 - paceMarkerCount
+        # always round away from zero
+        paceMarkerCount = int(ceil(abs(paceTotalSeconds / 15.0)))
+            
+        # We will display at most 9 pace chars
+        if paceMarkerCount > 9:
+            paceMarkerCount = 9
+        # we have 20 chars, those that are not pace markers are spaces
+        paddCharCount = 20 - paceMarkerCount
 
-		# if we are ahead with positive paceTotalSeconds, use A
-		# if we are behaind with negative paceTotalSeconds, use B
-		if paceTotalSeconds > 0: 
-			paceMarkerChar = "A"
-		else:
-			paceMarkerChar = "B"
+        # if we are ahead with positive paceTotalSeconds, use A
+        # if we are behaind with negative paceTotalSeconds, use B
+        if paceTotalSeconds > 0: 
+            paceMarkerChar = "A"
+        else:
+            paceMarkerChar = "B"
 
-		paceString = ""
+        paceString = ""
 
-		# build correct length pace String
-		for i in range(1, paceMarkerCount):
-			paceString += paceMarkerChar
+        # build correct length pace String
+        for i in range(1, paceMarkerCount):
+            paceString += paceMarkerChar
 
-		# build correct length padd string and place it on the correct side
-		#if paceTotalSeconds > 0:
-		if paceTotalSeconds > 0:
-			paddString = ""
-			for i in range(1, paddCharCount):
-				paddString += " "
-			paceString = paddString + paceString
+        # build correct length padd string and place it on the correct side
+        #if paceTotalSeconds > 0:
+        if paceTotalSeconds > 0:
+            paddString = ""
+            for i in range(1, paddCharCount):
+                paddString += " "
+            paceString = paddString + paceString
 
-		line4 = paceString +  "\r\n"
+        line4 = paceString +  "\r\n"
 
-		# sys.stdout.write(line1)
-		# sys.stdout.write(line2)
-		# sys.stdout.write(line3)
-		# sys.stdout.write(line4)
-		self.lcd.clear()
-		self.lcd.write_string(line1)
-		self.lcd.write_string(line2)
-		self.lcd.write_string(line3)
-		self.lcd.write_string(line4)
-	# def lcdTest1(self):
-	# 	#                 12345678911234567892
-	# 	self.lcd.write_string('D12.34   P+15:35  RS')
-	# 	self.lcd.write_string('                  AS')
-	# 	self.lcd.write_string('T 09:00   NP 1.5  CS')
-	# 	self.lcd.write_string('BBBBBBBBB  AAAAAAAAA')
+        # sys.stdout.write(line1)
+        # sys.stdout.write(line2)
+        # sys.stdout.write(line3)
+        # sys.stdout.write(line4)
+        self.lcd.clear()
+        self.lcd.write_string(line1)
+        self.lcd.write_string(line2)
+        self.lcd.write_string(line3)
+        self.lcd.write_string(line4)
+        
+        
+    # def lcdTest1(self):
+    #     #                 12345678911234567892
+    #     self.lcd.write_string('D12.34   P+15:35  RS')
+    #     self.lcd.write_string('                  AS')
+    #     self.lcd.write_string('T 09:00   NP 1.5  CS')
+    #     self.lcd.write_string('BBBBBBBBB  AAAAAAAAA')
